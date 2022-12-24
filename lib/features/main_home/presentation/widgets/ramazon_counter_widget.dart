d4232786a97d2f309072195f638c9df027b69b7e
Boburbek <boburbekking@gmail.com>
2022-12-24 11:45:42 +0500

    Boburbek 7-commit, onPressed funksion added

--- lib/features/main_home/presentation/widgets/ramazon_counter_widget.dart
+++ lib/features/main_home/presentation/widgets/ramazon_counter_widget.dart
@@ -6,8 +6,8 @@ class RamazonCounterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 90,
-      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 40),
+      height: 70,
+      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: Colors.green.shade800.withOpacity(0.6),
